d564355555dc6a2e30c785a2470cec245773cfa5
ManishKumar-13 <kumarmanish70175@gmail.com>
2024-05-03 13:45:03 +0530

    Initial Commit

---
+++ src/components/CardSection.jsx
@@ -0,0 +1,162 @@
+function CardSection() {
+  return (
+    <div className="cards">
+      <div className="card">
+
+        <div className="card-header">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro aut
+            voluptate, ab maxime non blanditiis laudantium dignissimos
+            voluptatum fuga nemo?
+          </p>
+        </div>
+
+        <div className="profile">
+            <img src="https://source.unsplash.com/1600x1600/?working+man+profile+pic" alt="" className="profile-pic" />
+            <div className="designation">
+                <h4>Gladis Lennon</h4>
+                <p>Head of SEO</p>
+            </div>
+        </div>
+      </div>
+
+      <div className="card">
+
+        <div className="card-header">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro aut
+            voluptate, ab maxime non blanditiis laudantium dignissimos
+            voluptatum fuga nemo?
+          </p>
+        </div>
+
+        <div className="profile">
+            <img src="https://source.unsplash.com/1600x1600/?working+man+profile+pic" alt="" className="profile-pic" />
+            <div className="designation">
+                <h4>Gladis Lennon</h4>
+                <p>Head of SEO</p>
+            </div>
+        </div>
+      </div>
+
+      <div className="card">
+
+        <div className="card-header">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro aut
+            voluptate, ab maxime non blanditiis laudantium dignissimos
+            voluptatum fuga nemo?
+          </p>
+        </div>
+
+        <div className="profile">
+            <img src="https://source.unsplash.com/1600x1600/?working+man+profile+pic" alt="" className="profile-pic" />
+            <div className="designation">
+                <h4>Gladis Lennon</h4>
+                <p>Head of SEO</p>
+            </div>
+        </div>
+      </div>
+
+      <div className="card">
+
+        <div className="card-header">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro aut
+            voluptate, ab maxime non blanditiis laudantium dignissimos
+            voluptatum fuga nemo?
+          </p>
+        </div>
+
+        <div className="profile">
+            <img src="https://source.unsplash.com/1600x1600/?working+man+profile+pic" alt="" className="profile-pic" />
+            <div className="designation">
+                <h4>Gladis Lennon</h4>
+                <p>Head of SEO</p>
+            </div>
+        </div>
+      </div>
+
+
+      <div className="card">
+
+        <div className="card-header">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro aut
+            voluptate, ab maxime non blanditiis laudantium dignissimos
+            voluptatum fuga nemo?
+          </p>
+        </div>
+
+        <div className="profile">
+            <img src="https://source.unsplash.com/1600x1600/?working+man+profile+pic" alt="" className="profile-pic" />
+            <div className="designation">
+                <h4>Gladis Lennon</h4>
+                <p>Head of SEO</p>
+            </div>
+        </div>
+      </div>
+
+
+      <div className="card">
+
+        <div className="card-header">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro aut
+            voluptate, ab maxime non blanditiis laudantium dignissimos
+            voluptatum fuga nemo?
+          </p>
+        </div>
+
+        <div className="profile">
+            <img src="https://source.unsplash.com/1600x1600/?working+man+profile+pic" alt="" className="profile-pic" />
+            <div className="designation">
+                <h4>Gladis Lennon</h4>
+                <p>Head of SEO</p>
+            </div>
+        </div>
+      </div>
+
+
+      <div className="card">
+
+        <div className="card-header">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro aut
+            voluptate, ab maxime non blanditiis laudantium dignissimos
+            voluptatum fuga nemo?
+          </p>
+        </div>
+
+        <div className="profile">
+            <img src="https://source.unsplash.com/1600x1600/?working+man+profile+pic" alt="" className="profile-pic" />
+            <div className="designation">
+                <h4>Gladis Lennon</h4>
+                <p>Head of SEO</p>
+            </div>
+        </div>
+      </div>
+
+      <div className="card">
+
+        <div className="card-header">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro aut
+            voluptate, ab maxime non blanditiis laudantium dignissimos
+            voluptatum fuga nemo?
+          </p>
+        </div>
+
+        <div className="profile">
+            <img src="https://source.unsplash.com/1600x1600/?working+man+profile+pic" alt="" className="profile-pic" />
+            <div className="designation">
+                <h4>Gladis Lennon</h4>
+                <p>Head of SEO</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardSection;
